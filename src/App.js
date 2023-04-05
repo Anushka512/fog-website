f27fc2891ad4d2891c3fe7c03f6d428ced6d4c3e
Deepak Yadav <deepakydv0001@gmail.com>
2023-04-05 20:49:35 +0530

    first

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import "./style.scss";
+// import Navbar from './Components/Navbar.js';
+// import { Routes } from "react-router-dom";
+import Error from "./Pages/Error/Error.js"
+
+function App() {
+  return (
+    <div className="App">
+        {/* <Navbar /> */}
+        <Error/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Navbar/Navbar.js
@@ -0,0 +1,11 @@
+import '../style.css';
+// import {link} from "react-router-dom"
+import "./Navbar.scss"
+
+export default function navbar() {
+  return (
+    <nav className='navbar-items'>
+      <h1 className='navbar-logo'> Free of Gluten</h1>
+    </nav>
+  );
+}
