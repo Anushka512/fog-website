3b79d600f574c262b2a4b404c1704b7e31cf3131
Hammad-4846 <curious4846@gmail.com>
2023-04-07 23:03:47 +0530

    Added Routes

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,11 +1,47 @@
-import React from 'react'
+import React, { Fragment } from "react";
 
 import "./Home.scss";
 
 function Home() {
   return (
-    <div>Home</div>
-  )
+    <Fragment>
+      <div className="banner">
+        <div className="container home__h-info">
+          <h1>
+            It’s not a dream. Gluten-free <br /> is finally real!
+          </h1>
+          <button className="btn cart">Shop Now</button>
+        </div>
+      </div>
+
+      <article className="services__option">
+        <div className="ser ser-1">
+          <p>
+            <i class="fa-regular fa-truck-fast"></i>
+          </p>
+          <h4>Free Delivery</h4>
+          <p className="desc">on orders above ₹999</p>
+        </div>
+        
+        <div className="ser ser-2">
+          <p>
+            <i class="fa-regular fa-truck-fast"></i>
+          </p>
+          <h4>Free Delivery</h4>
+          <p className="desc">on orders above ₹999</p>
+        </div>
+
+        <div className="ser ser-3">
+          <p>
+            <i class="fa-regular fa-truck-fast"></i>
+          </p>
+          <h4>Free Delivery</h4>
+          <p className="desc">on orders above ₹999</p>
+        </div>
+
+      </article>
+    </Fragment>
+  );
 }
 
-export default Home
+export default Home;
