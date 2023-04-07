2c0e5bd95ecd7c88b5be61ea106d4c0eabb85da4
Deepak Yadav <deepakydv0001@gmail.com>
2023-04-07 20:31:16 +0530

    local

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import "./style.scss";
+import React from "react";
 // import Navbar from './Components/Navbar.js';
 // import { Routes } from "react-router-dom";
 import Error from "./Pages/Error/Error.js"
-import Footer from "./Components/Footer/Footer"
+import Footer from "./Components/Footer/Footer.js"
 
 function App() {
   return (
     <div className="App">
       {/* <Navbar /> */}
-      <Error />
+       <Error />
       <Footer />
+      Hii
     </div>
   );
 }
