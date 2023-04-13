d2dd955d2e3741f74080a9ba0de40bb5f948eb3a
Hammad-4846 <curious4846@gmail.com>
2023-04-13 23:23:17 +0530

    Navbar Updated

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,29 +1,44 @@
-// import '../style.css';
-// import {link} from "react-router-dom"
-import Logo from "../../Assets/Images/Group.png";
-import User from "../../Assets/Images/Ellipse 6.png";
+
+import Logo from "../../Assets/Images/logo__sec.png";
+import { AiOutlineShoppingCart, AiOutlineHeart } from "react-icons/ai";
+import { BsFacebook, BsInstagram, BsTwitter } from "react-icons/bs";
 import "./Navbar.scss";
 
 export default function Navbar() {
   return (
-    <nav className="navbar-items">
-      <div className="container nav__container">
-        <div className="logo">
-          <img src={Logo} alt="Logo" />
+    <div className="wrapper__nav">
+      <div className="socials__nav">
+        <div className="social__icon">
+          <BsFacebook />
+          <BsInstagram />
+          <BsTwitter />
         </div>
-        <ul>
-          {["Home", "About Us", "Blog", "Contact Us"].map((item, id) => (
-            <li key={item - id}>
-              <a href={`#${item}`}>{item}</a>
-            </li>
-          ))}
-          <a className="btn cart">Your Cart</a>
-          <a className="btn wishlist">Wish List</a>
-          <div className="profile">
-            <img src={User} alt="userProfile" />
-          </div>
-        </ul>
+
+        <p className="p-text">
+          Sale: 20% off on orders above ₹999. Valid until 22nd April only.
+        </p>
       </div>
-    </nav>
+      <nav className="navbar-items">
+        <div className="container nav__container">
+          <div className="logo">
+            <img src={Logo} alt="Logo" />
+          </div>
+          <ul>
+            {["Home", "About Us", "Blog", "Contact Us"].map((item, id) => (
+              <li key={item - id}>
+                <a href={`#${item}`}>{item}</a>
+              </li>
+            ))}
+          </ul>
+          <div className="right">
+            <AiOutlineShoppingCart />
+            <AiOutlineHeart />
+            <div className="auth">
+              <button className="btn cart">Login/Signup</button>
+            </div>
+          </div>
+        </div>
+      </nav>
+    </div>
   );
 }
